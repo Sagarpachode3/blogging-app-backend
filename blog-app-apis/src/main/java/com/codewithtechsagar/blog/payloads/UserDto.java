009edd16060f219836f8e21cb8a8529a1d824256
“Sagar_Pachode3” <sagarpachode3@gmail.com>
2024-01-12 09:35:49 +0530

    Bean Validation using hibernate validator for User API.

--- blog-app-apis/src/main/java/com/codewithtechsagar/blog/payloads/UserDto.java
+++ blog-app-apis/src/main/java/com/codewithtechsagar/blog/payloads/UserDto.java
@@ -1,5 +1,8 @@
 package com.codewithtechsagar.blog.payloads;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,11 +14,19 @@ public class UserDto {
 
 	private int id;
 
+	@NotNull
+	@NotEmpty
 	private String name;
 
+	@Email
+	@NotEmpty
 	private String email;
 
+	@NotNull
+	@NotEmpty
 	private String password;
 
+	@NotNull
+	@NotEmpty
 	private String about;
 }
