19e314c9fde389af4dd4e4b527bda94ec6b5a45e
“Sagar_Pachode3” <sagarpachode3@gmail.com>
2024-01-19 22:18:39 +0530

    New Feature added for file upload and for fetching the file. Worked on FileServices class. Also included Image extension verification method.

--- blog-app-apis/src/main/java/com/codewithtechsagar/blog/controllers/PostController.java
+++ blog-app-apis/src/main/java/com/codewithtechsagar/blog/controllers/PostController.java
@@ -1,10 +1,15 @@
 package com.codewithtechsagar.blog.controllers;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,12 +19,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.codewithtechsagar.blog.config.AppConstants;
 import com.codewithtechsagar.blog.payloads.ApiResponse;
 import com.codewithtechsagar.blog.payloads.PostDto;
 import com.codewithtechsagar.blog.payloads.PostResponse;
+import com.codewithtechsagar.blog.services.FileService;
 import com.codewithtechsagar.blog.services.PostService;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/api")
@@ -27,6 +36,12 @@ public class PostController {
 
 	@Autowired
 	private PostService postService;
+
+	@Autowired
+	private FileService fileService;
+
+	@Value("${project.image}")
+	private String path;
 
 	// Create Post
 
@@ -63,16 +78,15 @@ public class PostController {
 	// get All posts
 	@GetMapping("/posts")
 	ResponseEntity<PostResponse> getAllPosts(
-			@RequestParam (value = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
+			@RequestParam(value = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
 			@RequestParam(value = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize,
-			@RequestParam(value="sortBy", defaultValue = AppConstants.SORT_BY, required = false) String sortBy,
-			@RequestParam(value="sortDir", defaultValue = AppConstants.SORT_DIR, required = false) String sortDir
-			) {
+			@RequestParam(value = "sortBy", defaultValue = AppConstants.SORT_BY, required = false) String sortBy,
+			@RequestParam(value = "sortDir", defaultValue = AppConstants.SORT_DIR, required = false) String sortDir) {
 		PostResponse allPosts = this.postService.getAllPosts(pageNumber, pageSize, sortBy, sortDir);
 		return new ResponseEntity<PostResponse>(allPosts, HttpStatus.OK);
 	}
 
-	// get posts by user	
+	// get posts by user
 
 	@GetMapping("/user/{userId}/posts")
 	ResponseEntity<List<PostDto>> getPostsByUser(@PathVariable Integer userId) {
@@ -89,13 +103,42 @@ public class PostController {
 
 		return new ResponseEntity<List<PostDto>>(postsByCategory, HttpStatus.OK);
 	}
-	
-	//Search
-	
+
+	// Search
+
 	@GetMapping("/posts/search/{keywords}")
-	ResponseEntity<List<PostDto>> searchPostByTitle(
-			@PathVariable("keywords") String keywords){
+	ResponseEntity<List<PostDto>> searchPostByTitle(@PathVariable("keywords") String keywords) {
 		List<PostDto> result = this.postService.searchPosts(keywords);
 		return new ResponseEntity<List<PostDto>>(result, HttpStatus.OK);
+	}
+
+	// post image upload
+
+	@PostMapping("/post/image/upload/{postId}")
+	public ResponseEntity<PostDto> uploadPostImage(@RequestParam("image") MultipartFile image,
+			@PathVariable Integer postId) throws IOException {
+
+		PostDto postDto = this.postService.getPostById(postId);
+
+		String fileName = this.fileService.uploadImage(path, image);
+
+		postDto.setImageName(fileName);
+		PostDto updatePost = this.postService.updatePost(postDto, postId);
+
+		return new ResponseEntity<PostDto>(updatePost, HttpStatus.OK);
+
+	}
+ 
+	// method to serve files
+
+	@GetMapping(value = "/post/image/{imageName}", produces = MediaType.IMAGE_JPEG_VALUE)
+	public void downloadImage(@PathVariable("imageName") String imageName, HttpServletResponse response)
+			throws IOException {
+
+		InputStream resource = this.fileService.getResource(path, imageName);
+
+		response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+
+		StreamUtils.copy(resource, response.getOutputStream());
 	}
 }
--- blog-app-apis/src/main/java/com/codewithtechsagar/blog/services/impl/FileServiceImpl.java
+++ blog-app-apis/src/main/java/com/codewithtechsagar/blog/services/impl/FileServiceImpl.java
@@ -1,26 +1,89 @@
 package com.codewithtechsagar.blog.services.impl;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.UUID;
 
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.codewithtechsagar.blog.services.FileService;
 
+@Service
 public class FileServiceImpl implements FileService {
 
 	@Override
 	public String uploadImage(String path, MultipartFile file) throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		// File name
+		String name = file.getOriginalFilename();
+		// abc.png
+
+		// Validate file type
+		if (!isValidImageType(file.getContentType(), name)) {
+			throw new IllegalArgumentException("Invalid image type. Allowed types are PNG, JPEG, or JPG.");
+		}
+
+		// random name generate file
+		String randomId = UUID.randomUUID().toString();
+		String fileName1 = randomId.concat(name.substring(name.lastIndexOf(".")));
+
+		// Fullpath
+		String filePath = path + File.separator + fileName1;
+
+		// create folder if not created
+		File f = new File(path);
+		if (!f.exists()) {
+			f.mkdir();
+		}
+
+		// file Copy
+		Files.copy(file.getInputStream(), Paths.get(filePath));
+
+		return fileName1;
 	}
 
 	@Override
 	public InputStream getResource(String path, String fileName) throws FileNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		String fullPath = path + File.separator + fileName;
+		InputStream is = new FileInputStream(fullPath);
+
+		return is;
+	}
+
+	private boolean isValidImageType(String contentType, String fileName) {
+
+		// Check if the content type or file extension indicates an image of type PNG,
+		// JPEG, or JPG
+
+		return contentType != null
+				&& (contentType.startsWith("image/png") || contentType.startsWith("image/jpeg")
+						|| contentType.startsWith("image/jpg"))
+				&& StringUtils.getFilenameExtension(fileName).matches("(?i)(png|jpe?g)");
+
+		// OR
+
+		/*
+		 * private boolean isValidImageType(String fileName) { 
+		 * String extension = StringUtils.getFilenameExtension(fileName);
+		 * 
+		 * // Check if the extension indicates an image of type PNG, JPEG, or JPG
+		 * 
+		 * return extension != null && extension.matches("(?i)(png|jpe?g)"); }
+		 OR
+		 * return extension != null && fileName.matches("(?i).+\\.(png|jpeg|jpg)");
+		 * 
+		 * (?i): Case-insensitive matching. 
+		 * .+:: Matches one or more characters (any character) before the dot (.) in the file extension. 
+		 * \\.: Escaped dot (.) to match the actual dot in the file extension.
+		 *  (png|jpeg|jpg): Alternation to match either "png", "jpeg", or "jpg".
+		 * 
+		 */
 	}
 
 }
- 
