4327af9c190a420da0a1aed0ea03219fd94503c0
“Sagar_Pachode3” <sagarpachode3@gmail.com>
2024-01-23 22:25:11 +0530

    Worked on Basic spring security, Created ManyToMany relationship between Role and User. Created customUserService

--- blog-app-apis/src/main/java/com/codewithtechsagar/blog/BlogAppApisApplication.java
+++ blog-app-apis/src/main/java/com/codewithtechsagar/blog/BlogAppApisApplication.java
@@ -1,13 +1,19 @@
 package com.codewithtechsagar.blog;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class BlogAppApisApplication {
+public class BlogAppApisApplication implements CommandLineRunner {
 
+	@Autowired
+	private PasswordEncoder passwordencoder;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BlogAppApisApplication.class, args);
 	}
@@ -16,5 +22,11 @@ public class BlogAppApisApplication {
 	public ModelMapper modelMapper() {
 	
 		return new ModelMapper();
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(this.passwordencoder.encode("xyz"));
+		
 	}
 }
